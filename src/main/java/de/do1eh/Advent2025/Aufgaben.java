889472f65a9c4872278b28a672ae18f140e670f1
Ralf Lüsebrink <rnh3@gmx.net>
2025-12-07 19:27:06 +0100

    SESSIOM-COOKIE erklärt

--- src/main/java/de/do1eh/Advent2025/Aufgaben.java
+++ src/main/java/de/do1eh/Advent2025/Aufgaben.java
@@ -4,7 +4,13 @@ import java.util.*;
 
 import static de.do1eh.Advent2025.Session.SESSION_COOKIE;
 
-
+/**
+ * Achtung: Zum Ausführen benötigt man noch das SESSION_COOKIE
+ * Dieses findet man indem man sich mit einem beliebigen browser bei Adventofcode.com einloggt,
+ * Mit F12 die Dev Tools aufruft und sucht dort unter Application->Cookies  den Wert von 'session'.
+ * Diesen mit STRG+C kopieren und einer Variablen
+ * public final static String SESSION_COOKIE = "xxxxxx"; zur Verügung stellen.
+ */
 public class Aufgaben {
     int position=0;
      final Map<String, Long> memo = new HashMap<>();
