977d91a2a76838e87eba7f66f1932d53455d634c
Ralf Lüsebrink <rnh3@gmx.net>
2025-12-13 15:49:36 +0100

    Tag10 part1 fertig

--- src/main/java/de/do1eh/Advent2025/Day10.java
+++ src/main/java/de/do1eh/Advent2025/Day10.java
@@ -13,12 +13,14 @@ public class Day10 {
 
     public void part1() {
 
-        //List<String> input = readUrlContent("https://adventofcode.com/2025/day/9/input", SESSION_COOKIE);
+        int loesung=0;
+        List<String> input = readUrlContent("https://adventofcode.com/2025/day/10/input", SESSION_COOKIE);
+        /*
         List<String> input = new ArrayList<String>();
         input.add("[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}");
         input.add("[...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}");
         input.add("[.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}");
-
+*/
 
         for (int i = 0; i < input.size(); i++) {
 
@@ -53,32 +55,82 @@ public class Day10 {
                 //schalteeints durchlaufen und in bitmuster ändern
                 String bitmuster="";
                 for (int pos:schalterint) {
+                    //System.out.println("schalterint:"+pos);
                     while(pos>bitmuster.length()) {bitmuster+="0";}
                     bitmuster+="1";
                 }
-                System.out.println("schalterresultat:"+bitmuster);
+                while (bitmuster.length()<lampenstr.length()) {
+                    bitmuster+=0;
+                }
+                //System.out.println("schalterresultat:"+bitmuster);
                 //bitmuster speichern
                 schalter.add(Integer.parseInt(bitmuster, 2));
              }
             //String joltage = input.get(i).substring(input.get(i).indexOf('{') + 1, input.get(i).indexOf('}'));
 
-            System.out.println("lampe:"+lampe);
-            System.out.println("lampestring:"+lampenstr);
+            //System.out.println("lampe:"+lampe);
+            //System.out.println("lampestring:"+lampenstr);
             //System.out.println(1 << lampenstr.length());
-            System.out.println("schalterresultatliste"+schalter);
+            //System.out.println("schalterresultatliste"+schalter);
             //System.out.println(joltage);
 
             //Drückalgo:  Alle Kombinationen aus der Schalterresultatliste durchgehen, per xor verknüpfen und mit
             //Ziel vergleichen Wenn Ergebnis==Ziel -> anzahl benutzte schalter speichern
             //Am Ende: loesung+=kleinste Anzahl Schalter dieser Runde
+            List<List<Integer>> allekombinationen= generateSubsets(schalter);
+            //System.out.println(allekombinationen);
 
-
-
-
-
-
-
-/
+            int anzahlknopfdruecke=0;
+            for (List<Integer> kombinationen:allekombinationen) {
+                Integer xor=0;
+                for (Integer knopfdruck:kombinationen) {
+                    xor^= knopfdruck;
+                 }
+                if (xor==lampe && (anzahlknopfdruecke==0 ||anzahlknopfdruecke>kombinationen.size())) {
+                    anzahlknopfdruecke=kombinationen.size();
+                    //System.out.println("xor:"+Integer.toBinaryString(xor));
+                    //System.out.println(kombinationen);
+                }
+            }
+            //System.out.println("anzahl:"+anzahlknopfdruecke);
+           loesung+=anzahlknopfdruecke;
         }
+        System.out.println("Loesung:"+loesung);
+    }
+
+    public static List<List<Integer>> generateSubsets(List<Integer> numbers) {
+        List<List<Integer>> subsets = new ArrayList<>();
+        int n = numbers.size();
+
+        // Die Gesamtzahl der möglichen Teilmengen (einschließlich der leeren Menge)
+        // ist 2^n, wobei n die Größe der Liste ist.
+        int totalSubsets = 1 << n; // Das ist gleich 2 hoch n (2^n)
+/*
+        System.out.println("--- Generiere Teilmengen ---");
+        System.out.println("Listenlänge (n): " + n);
+        System.out.println("Anzahl Teilmengen (2^n): " + totalSubsets);
+        System.out.println("----------------------------");
+*/
+
+        // Wir durchlaufen alle Zahlen von 0 bis 2^n - 1. Jede Zahl ist eine Bitmaske.
+        for (int i = 0; i < totalSubsets; i++) {
+            List<Integer> currentSubset = new ArrayList<>();
+
+            // Überprüfe jedes Element im Eingabearray
+            for (int j = 0; j < n; j++) {
+
+                // Bitmasken-Logik:
+                // Wenn das j-te Bit in der Zahl 'i' gesetzt ist (d.h. 1),
+                // dann gehört das Element numbers[j] zur aktuellen Teilmenge.
+                if ((i & (1 << j)) != 0) {
+                    currentSubset.add(numbers.get(j));
+                }
+            }
+
+            // Füge die gefundene Teilmenge zur Gesamtliste hinzu
+            subsets.add(currentSubset);
+        }
+
+        return subsets;
     }
 }
